52a894ec7f92750700f7831b91726c849bb9d4ad
benjamin0906@gmail.com <benjamin0906@gmail.com>
2020-05-20 15:42:15 +0200

    Just some formatting have performed.

--- Inc/types.h
+++ Inc/types.h
@@ -8,14 +8,14 @@
 #ifndef TYPES_H_
 #define TYPES_H_
 
-typedef unsigned long long uint64;
-typedef unsigned long	uint32;
-typedef unsigned short	uint16;
-typedef unsigned char	uint8;
-typedef signed long 	int32;
-typedef signed short	int16;
-typedef signed char		int8;
+typedef unsigned long long 	uint64;
+typedef unsigned long		uint32;
+typedef unsigned short		uint16;
+typedef unsigned char		uint8;
+typedef signed long 		int32;
+typedef signed short		int16;
+typedef signed char			int8;
 
-typedef float			float32;
+typedef float				float32;
 
 #endif /* TYPES_H_ */
