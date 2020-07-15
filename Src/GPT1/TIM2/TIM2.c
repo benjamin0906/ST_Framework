f55b4af182a2586bc3144d200eea29711d4021c2
Benjamin Bodnar <benjamin0906@gmail.com>
2020-07-15 08:55:56 +0200

    The compile witches in the previously uploaded memory map have been tried out

---
+++ Src/GPT1/TIM2/TIM2.c
@@ -0,0 +1,11 @@
+/*
+ * TIM2.c
+ *
+ *  Created on: 2020. júl. 15.
+ *      Author: BodnarB
+ */
+#define TIM2
+#include "../GPT_TIM_Types.h"
+
+static dtGPT1 *const TIM2_MAP = (dtGPT1*)(0x40000000);
+
