3ca9cb1e111ae517004a1dfe4d524e3f9a2382f9
BodnarB <benjamin.bodnar@balluff.hu>
2020-09-08 07:53:32 +0200

    Empty usart_types file has been added.

---
+++ Src/USART/USART_Types.h
@@ -0,0 +1,13 @@
+/*
+ * USART_Types.h
+ *
+ *  Created on: 2020. sep. 8.
+ *      Author: BodnarB
+ */
+ 
+#ifndef USART_USART_TYPES_H_
+#define USART_USART_TYPES_H_
+
+#include "types.h"
+
+#endif /* USART_USART_TYPES_H_ */
