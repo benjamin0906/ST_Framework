49d81f1d81b951c39ede09a83a1485a9140cdf68
BodnarB <benjamin.bodnar@balluff.hu>
2020-09-24 09:52:21 +0200

    Up to the third RCC peripheral registers of the F446, F410 and G070 have been added.

--- Inc/RCC.h
+++ Inc/RCC.h
@@ -12,20 +12,39 @@
 
 typedef enum
 {
+#if defined(MCU_F410) || defined(MCU_F446) || defined(MCU_G070)
 	RCC_GPIOA 	= 0,
-	RCC_GPIOB,
-	RCC_GPIOC,
-	RCC_GPIOD,
-	RCC_GPIOE,
-	RCC_GPIOF,
-	RCC_GPIOG,
-	RCC_GPIOH 	= 7,
-	RCC_CRC		= 12,
-#if defined(MCU_F410)
-	RCC_BKPSRAMEN	= 18,
 #endif
+#if defined(MCU_F410) || defined(MCU_F446) || defined(MCU_G070)
+	RCC_GPIOB	= 1,
+#endif
+#if defined(MCU_F410) || defined(MCU_F446) || defined(MCU_G070)
+	RCC_GPIOC	= 2,
+#endif
+#if defined(MCU_F446) || defined(MCU_G070)
+	RCC_GPIOD	= 3,
+#endif
+#if defined(MCU_F446)
+	RCC_GPIOE	= 4,
+#endif
+#if defined(MCU_F446) || defined(MCU_G070)
+	RCC_GPIOF	= 5,
+#endif
+#if defined(MCU_F446)
+	RCC_GPIOG	= 6,
+#endif
+#if defined(MCU_F410) || defined(MCU_F446)
+	RCC_GPIOH 	= 7,
+#endif
+#if defined(MCU_F410) || defined(MCU_F446)
+	RCC_CRC		= 12,
+#endif
+#if defined(MCU_F410) || defined(MCU_F446)
 	RCC_DMA1	= 21,
+#endif
+#if defined(MCU_F410) || defined(MCU_F446)
 	RCC_DMA2	= 22,
+#endif
 #if defined(MCU_F446)
 	RCC_OTGHS	= 29,
 #endif
@@ -34,11 +53,118 @@ typedef enum
 #endif
 #if defined(MCU_F446)
 	RCC_DCMI	= 32,
+#elif defined(MCU_G070)
+	RCC_DMA		= 32,
+#endif
+#if defined(MCU_F410)
+	RCC_TIM5	= 35,
+#endif
+#if defined(MCU_F410)
+	RCC_TIM6	= 36,
+#endif
+#if defined(MCU_F446)
 	RCC_OTGFS	= 39,
+#endif
+#if defined(MCU_G070)
+	RCC_FLASH	= 40,
+#endif
+#if defined(MCU_F410)
+	RCC_LPTIM1	= 41,
+#endif
+#if defined(MCU_F410)
+	RCC_WWDG	= 43,
+#endif
+#if defined(MCU_G070)
+	RCC_CRC		= 44,
+#endif
+#if defined(MCU_F410)
+	RCC_SPI2	= 46,
+#endif
+#if defined(MCU_F410)
+	RCC_USART2	= 49,
+#endif
+#if defined(MCU_F410)
+	RCC_I2C1	=53,
+#endif
+#if defined(MCU_F410)
+	RCC_I2C2	=54,
+#endif
+#if defined(MCU_F410)
+	RCC_I2C4	=56,
+#endif
+#if defined(MCU_F410)
+	RCC_PWR		=60,
+#endif
+#if defined(MCU_F410)
+	RCC_DAC		=61,
+#endif
+#if defined(MCU_F446)
 	RCC_FMC		= 64,
+#elif defined(MCU_F410)
+	RCC_TIM1	= 64,
+#endif
+#if defined(MCU_F446)
 	RCC_QSPI	= 65,
+#elif defined(MCU_G070)
+	RCC_TIM3	= 65,
+#endif
+#if defined(MCU_F410)
+	RCC_USART1	= 68,
+#elif defined(MCU_G070)
+	RCC_TIM6	= 68,
+#endif
+#if defined(MCU_F410)
+	RCC_USART6	= 69,
+#elif defined(MCU_G070)
+	RCC_TIM7	= 69,
+#endif
+#if defined(MCU_F410)
+	RCC_ADC1	= 72,
+#endif
+#if defined(MCU_F410)
+	RCC_SPI1	= 76,
+#endif
+#if defined(MCU_F410)
+	RCC_SYSCFG	= 78,
+#elif defined(MCU_G070)
+	RCC_SPI2	= 78,
+#endif
+#if defined(MCU_F410)
+	RCC_TIM9	= 80,
+#endif
+#if defined(MCU_G070)
+	RCC_USART2	= 81,
+#endif
+#if defined(MCU_F410)
+	RCC_TIM11	= 82,
+#elif defined(MCU_G070)
+	RCC_USART3	= 82,
+#endif
+#if defined(MCU_G070)
+	RCC_USART4	= 83,
+#endif
+#if defined(MCU_F410)
+	RCC_SPI5	= 84,
+#endif
+#if defined(MCU_G070)
+	RCC_I2C1	= 85,
+#endif
+#if defined(MCU_G070)
+	RCC_I2C2	= 86,
+#endif
+#if defined(MCU_G070)
+	RCC_DBG		= 91,
+#endif
+#if defined(MCU_G070)
+	RCC_PWR		= 92,
+#endif
+#if defined(MCU_F446)
 	RCC_TIM2	= 128,
+#endif
+#if defined(MCU_F446)
 	RCC_TIM3	= 129,
+#endif
+#if defined(MCU_F446)
 	RCC_TIM4	= 130,
 #endif
 	RCC_TIM5	= 131,
