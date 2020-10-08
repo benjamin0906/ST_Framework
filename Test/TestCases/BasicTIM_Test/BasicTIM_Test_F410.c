4f9650d2bbde43e7937cb90ec5773f564e9cf1b9
benjamin0906 <benjamin.bodnar@balluff.hu>
2020-10-08 14:44:33 +0200

    Implementation of the BasicTIM test has been started.

---
+++ Test/TestCases/BasicTIM_Test/BasicTIM_Test_F410.c
@@ -0,0 +1,16 @@
+/*
+ * BasicTIM_Test_F410.c
+ *
+ *  Created on: 2020. okt. 8.
+ *      Author: BodnarB
+ */
+
+#define TEST_CASE
+#include "TestEnv.h"
+
+void BasicTIM_Test(void);
+
+void BasicTIM_Test(void)
+{
+
+}
