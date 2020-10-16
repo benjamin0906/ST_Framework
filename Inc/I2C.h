2804190fed95145b3c98433740747d5e55f365b4
benjamin0906 <benjamin0906@gmail.com>
2020-10-16 14:43:50 +0200

    I2C Prototypes have been added.

---
+++ Inc/I2C.h
@@ -0,0 +1,24 @@
+/*
+ * I2C.h
+ *
+ *  Created on: 2020. okt. 16.
+ *      Author: BodnarB
+ */
+
+#ifndef INC_I2C_H_
+#define INC_I2C_H_
+
+#include "types.h"
+
+typedef enum
+{
+	I2C_Write,
+	I2C_Read,
+} dtI2cSessionType;
+
+typedef struct
+{
+
+} dtI2cConfig;
+
+#endif /* INC_I2C_H_ */
--- Src/I2C/I2C.c
+++ Src/I2C/I2C.c
@@ -6,3 +6,7 @@
  */
 
 #include "I2C_Types.h"
+#include "I2C.h"
+
+void I2C_Init(dtI2cConfig condig);
+void I2C_Start(dtI2cSessionType SessionType, uint8* RegisterAddress, uint8 RegisterLength, uint8* Data, uint8 DataLength);
