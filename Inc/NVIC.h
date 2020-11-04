5d847e57b17df7fa7b3bef265051d830fc45e3e9
benjamin0906 <benjamin0906@gmail.com>
2020-11-04 08:16:20 +0100

    G071 variant has been added to NVIC.

--- Inc/NVIC.h
+++ Inc/NVIC.h
@@ -12,44 +12,44 @@
 
 typedef enum
 {
-#if defined(MCU_F446) || defined(MCU_G070) || defined(MCU_F410) || defined(MCU_L433)
+#if defined(MCU_F446) || defined(MCU_G070) || defined(MCU_F410) || defined(MCU_L433) || defined(MCU_G071)
 	IRQ_WWDG 			= 0,
 #endif
-#if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
+#if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433) || defined(MCU_G071)
 	IRQ_PVD				= 1,
 #endif
 #if defined(MCU_F410) || defined(MCU_F410)
 	IRQ_EXTI21			= 2,
 #elif defined(MCU_F446)
 	IRQ_TAMP_STAMP		= 2,
-#elif defined(MCU_G070) || defined(MCU_L433)
+#elif defined(MCU_G070) || defined(MCU_L433) || defined(MCU_G071)
 	IRQ_RTC_TAMP		= 2,
 #endif
 #if defined(MCU_F410) || defined(MCU_F410)
 	IRQ_EXTI22			= 3,
 #elif defined(MCU_F446) || defined(MCU_L433)
 	IRQ_RTC_WKUP		= 3,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_FLASH			= 3,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
 	IRQ_FLASH			= 4,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_RCC				= 4,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
 	IRQ_RCC				= 5,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_EXTI0_1			= 5,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
 	IRQ_EXTI0			= 6,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_EXTI2_3			= 6,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
 	IRQ_EXTI1			= 7,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_EXTI4_15		= 7,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
@@ -57,38 +57,38 @@ typedef enum
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
 	IRQ_EXTI3			= 9,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_DMA_Ch1			= 9,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
 	IRQ_EXTI4			= 10,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_DMA_Ch2_3		= 10,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410)
 	IRQ_DMA1_Stream0	= 11,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_DMA_Ch4_5_6_7	= 11,
 #elif defined(MCU_L433)
 	IRQ_DMA_CH1			= 11,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410)
 	IRQ_DMA1_Stream1	= 12,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_ADC				= 12,
 #elif defined(MCU_L433)
 	IRQ_DMA_CH2			= 12,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410)
 	IRQ_DMA1_Stream2	= 13,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_TIM1_BRK_UP_TRG	= 13,
 #elif defined(MCU_L433)
 	IRQ_DMA_CH3			= 13,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410)
 	IRQ_DMA1_Stream3	= 14,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_TIM1_CC			= 14,
 #elif defined(MCU_L433)
 	IRQ_DMA_CH4			= 14,
@@ -100,51 +100,51 @@ typedef enum
 #endif
 #if defined(MCU_F446) || defined(MCU_F410)
 	IRQ_DMA1_Stream5	= 16,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_TIM3			= 16,
 #elif defined(MCU_L433)
 	IRQ_DMA_CH6			= 16,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410)
 	IRQ_DMA1_Stream6	= 17,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_TIM6			= 17,
 #elif defined(MCU_L433)
 	IRQ_DMA_CH7			= 17,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
 	IRQ_ADC				= 18,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_TIM7			= 18,
 #endif
 #if defined(MCU_F446) || defined(MCU_L433)
 	IRQ_CAN1_TX			= 19,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_TIM14			= 19,
 #endif
 #if defined(MCU_F446) || defined(MCU_L433)
 	IRQ_CAN1_RX0		= 20,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_TIM15			= 20,
 #endif
 #if defined(MCU_F446) || defined(MCU_L433)
 	IRQ_CAN1_RX1		= 21,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_TIM16			= 21,
 #endif
 #if defined(MCU_F446) || defined(MCU_L433)
 	IRQ_CAN1_SCE		= 22,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_TIM17			= 22,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
 	IRQ_EXTI9_5 		= 23,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_I2C1			= 23,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410)
 	IRQ_TIM1_BRK_TIM9	= 24,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_I2C2			= 24,
 #elif defined(MCU_L433)
 	IRQ_TIM1_BRK_TIM15	= 24,
@@ -153,38 +153,42 @@ typedef enum
 	IRQ_TIM1_UP			= 25,
 #elif defined(MCU_F446)
 	IRQ_TIM1_UP_TIM10	= 25,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_SPI1			= 25,
 #elif defined(MCU_L433)
 	IRQ_TIM1_UP_TIM16	= 25,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410)
 	IRQ_TIM1_TRG_COM_TIM11	= 26,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_SPI2				= 26,
 #elif defined(MCU_L433)
 	IRQ_TIM1_TRG_COM		= 26,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
 	IRQ_TIM1_CC				= 27,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_USART1				= 27,
 #endif
 #if defined(MCU_F446) || defined(MCU_L433)
 	IRQ_TIM2				= 28,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_USART2				= 28,
 #endif
 #if defined(MCU_F446) || defined(MCU_L433)
 	IRQ_TIM3				= 29,
-#elif defined(MCU_G070)
+#elif defined(MCU_G070) || defined(MCU_G071)
 	IRQ_USART3_4			= 29,
 #endif
 #if defined(MCU_F446)
 	IRQ_TIM4				= 30,
+#elif defined(MCU_G071)
+	IRQ_CEC					= 30,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
 	IRQ_I2C1_EV 			= 31,
+#elif defined(MCU_G071)
+	IRQ_AES_RNG				= 31,
 #endif
 #if defined(MCU_F446) || defined(MCU_F410) || defined(MCU_L433)
 	IRQ_I2C1_ER				= 32,
