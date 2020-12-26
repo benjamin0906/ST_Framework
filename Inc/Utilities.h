cea7061312175e8cb97c6678deaec9a15977b8c6
benjamin0906 <benjamin0906@gmail.com>
2020-12-26 14:15:03 +0100

    Double dabler functions have been added to the Utilities.

--- Inc/Utilities.h
+++ Inc/Utilities.h
@@ -15,6 +15,7 @@ extern float32 Power(uint8 Power, float32 Number);
 extern uint32 IsPassed(uint32 TimeStamp, uint32 Limit);
 extern uint8 StrEq(const uint8 *str1,const uint8 *str2);
 extern uint8 StrLen(const uint8 *const str);
+extern void Dabler8Bit(uint8 value, uint8 *Digits);
 
 #if defined(MCU_F446)
 extern float32 sqrt(float32 Number);
