58efa40ee49a0c1bfe20159add65b4802e584601
benjamin0906 <benjamin0906@gmail.com>
2021-04-07 22:00:27 +0200

    EXTI module has been implemented.

--- Inc/EXTI.h
+++ Inc/EXTI.h
@@ -8,6 +8,69 @@
 #ifndef INC_EXTI_H_
 #define INC_EXTI_H_
 
-extern void EXTI_Set(void);
+typedef enum
+{
+	EXTI_Exti0,
+	EXTI_Exti1,
+	EXTI_Exti2,
+	EXTI_Exti3,
+	EXTI_Exti4,
+	EXTI_Exti5,
+	EXTI_Exti6,
+	EXTI_Exti7,
+	EXTI_Exti8,
+	EXTI_Exti9,
+	EXTI_Exti10,
+	EXTI_Exti11,
+	EXTI_Exti12,
+	EXTI_Exti13,
+	EXTI_Exti14,
+	EXTI_Exti15,
+	EXTI_PVD_Out,
+	EXTI_COMP1_Out,
+	EXTI_COMP2_Out,
+	EXTI_RTC,
+	EXTI_TAMP 			= 21,
+	EXTI_I2C1_Wake		= 23,
+	EXTI_USART1_Wake 	= 25,
+	EXTI_USART2_Wake,
+	EXTI_CEC_Wake,
+	EXTI_LPUART1_Wake,
+	EXTI_LPTIM1,
+	EXTI_LPTIM2,
+	EXTI_LSE_CSS,
+	EXTI_UCPD1_Wake,
+	EXTI_UCPD2_Wake,
+} dtEXTILines;
+
+typedef enum
+{
+	EXTI_NoEdge,
+	EXTI_RisingEdge,
+	EXTI_FallingEdge,
+	EXTI_BothEdge,
+} dtEXTIEdge;
+
+typedef enum
+{
+	EXTI_PortA,
+	EXTI_PortB,
+	EXTI_PortC,
+	EXTI_PortD,
+	EXTI_PortF	= 0x05,
+} dtEXTIPort;
+
+typedef enum
+{
+	EXTI_NoMask,
+	EXTI_EventMask,
+	EXTI_InterruptMask,
+	EXTI_BothMask,
+} dtEXTIMask;
+
+extern void EXTI_LineSet(dtEXTILines Line, dtEXTIEdge EdgeType, dtEXTIPort PortType, dtEXTIMask ExtiMasks);
+extern uint8 EXTI_RisingPending(dtEXTILines Line);
+extern uint8 EXTI_FallingPending(dtEXTILines Line);
+extern void EXTI_ClearPending(dtEXTILines Line);
 
 #endif /* INC_EXTI_H_ */
--- Src/EXTI/EXTI.c
+++ Src/EXTI/EXTI.c
@@ -6,13 +6,78 @@
  */
 
 #include "EXTI_Types.h"
+#include "EXTI.h"
 
 static dtEXTI *const EXTI = (dtEXTI*)0x40021800;
 
-void EXTI_Set(void)
+void EXTI_LineSet(dtEXTILines Line, dtEXTIEdge EdgeType, dtEXTIPort PortType, dtEXTIMask ExtiMasks);
+uint8 EXTI_RisingPending(dtEXTILines Line);
+uint8 EXTI_FallingPending(dtEXTILines Line);
+void EXTI_ClearPending(dtEXTILines Line);
+
+void EXTI_LineSet(dtEXTILines Line, dtEXTIEdge EdgeType, dtEXTIPort PortType, dtEXTIMask ExtiMasks)
 {
-	EXTI->ExtiCr[4] = 0;
-	EXTI->GenConfEvents.RSTR.Fields.Lines = 0x10;
-	EXTI->IMR1 = 0x10;
-	EXTI->EMR1 = 0x10;
+	if(Line <= EXTI_COMP2_Out)
+	{
+		uint32 Mask = 1 << Line;
+		Mask = ~Mask;
+
+		/* Zeroing the line by default, it will set only if the EdgeType is not EXTI_NoEdge */
+		EXTI->GenConfEvents.RSTR.Fields.Lines &= Mask;
+		EXTI->GenConfEvents.FTSR.Fields.Lines &= Mask;
+		Mask = ~Mask;
+		if((EdgeType == EXTI_RisingEdge) || (EdgeType == EXTI_BothEdge)) EXTI->GenConfEvents.RSTR.Fields.Lines |= Mask;
+		if((EdgeType == EXTI_FallingEdge) || (EdgeType == EXTI_BothEdge)) EXTI->GenConfEvents.FTSR.Fields.Lines |= Mask;
+	}
+
+	/* Set the used port to the GPIO EXTI line if the Line is one of them */
+	if(Line <= EXTI_Exti15) EXTI->ExtiCr[Line] = PortType;
+
+	if(Line <= EXTI_LSE_CSS)
+	{
+		uint32 Mask = 1 << Line;
+		Mask = ~Mask;
+		EXTI->EMR1 &= Mask;
+		EXTI->IMR1 &= Mask;
+		Mask = ~Mask;
+		if((ExtiMasks == EXTI_EventMask) || (ExtiMasks == EXTI_BothMask))
+			{
+			uint32 *a = &EXTI->EMR1;
+			*a |= Mask;
+			EXTI->EMR1 |= Mask;
+			}
+		if((ExtiMasks == EXTI_InterruptMask) || (ExtiMasks == EXTI_BothMask)) EXTI->IMR1 |= Mask;
+	}
+	else
+	{
+		uint32 Mask = 1 << (Line-32);
+		Mask = ~Mask;
+		EXTI->EMR2 &= Mask;
+		EXTI->IMR2 &= Mask;
+		Mask = ~Mask;
+		if((ExtiMasks == EXTI_EventMask) || (ExtiMasks == EXTI_BothMask)) EXTI->EMR2 |= Mask;
+		if((ExtiMasks == EXTI_InterruptMask) || (ExtiMasks == EXTI_BothMask)) EXTI->IMR2 |= Mask;
+	}
+}
+
+
+/* The pending registers are only set if not only the event but the interrupt generation of the line is set.
+ * So if only the event generation has been set the pending registers will be empty */
+uint8 EXTI_RisingPending(dtEXTILines Line)
+{
+	return (EXTI->GenConfEvents.RPR.Word & (1<<Line)) != 0;
+}
+
+/* The pending registers are only set if not only the event but the interrupt generation of the line is set.
+ * So if only the event generation has been set the pending registers will be empty */
+uint8 EXTI_FallingPending(dtEXTILines Line)
+{
+	return (EXTI->GenConfEvents.FPR.Word & (1<<Line)) != 0;
+}
+
+/* Clears both rising and falling flag */
+void EXTI_ClearPending(dtEXTILines Line)
+{
+	EXTI->GenConfEvents.FPR.Word |= (1 << Line);
+	EXTI->GenConfEvents.RPR.Word |= (1 << Line);
 }
