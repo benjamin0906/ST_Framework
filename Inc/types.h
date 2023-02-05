1cdc832393c5eb202432f93268b31d2ffbc136c6
benjamin0906 <benjamin0906@gmail.com>
2023-02-05 21:07:52 +0100

    int64 type has been defined

--- Inc/types.h
+++ Inc/types.h
@@ -12,6 +12,7 @@ typedef unsigned long long 	uint64;
 typedef unsigned long		uint32;
 typedef unsigned short		uint16;
 typedef unsigned char		uint8;
+typedef signed long long    int64;
 typedef signed long 		int32;
 typedef signed short		int16;
 typedef signed char			int8;
