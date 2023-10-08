67144c62a2a9de7205a03b5ac3a6df023bca2ec4
benjamin0906 <benjamin0906@gmail.com>
2023-10-08 21:21:22 +0200

    Std_Types has been added, SysTick has been updated Function descriptions and compile time switches have been added to SysTick

---
+++ Inc/Std_Types.h
@@ -0,0 +1,26 @@
+/*
+ * Std_Types.h
+ *
+ *  Created on: 2023. okt. 8.
+ *      Author: Benjamin
+ */
+
+#ifndef INC_STD_TYPES_H_
+#define INC_STD_TYPES_H_
+
+typedef unsigned long long  uint64;
+typedef unsigned long       uint32;
+typedef unsigned short      uint16;
+typedef unsigned char       uint8;
+
+typedef signed long long    int64;
+typedef signed long         int32;
+typedef signed short        int16;
+typedef signed char         int8;
+
+typedef float               float32;
+
+#define STD_ON  1
+#define STD_OFF 0
+
+#endif /* INC_STD_TYPES_H_ */
--- Src/SysTick/SysTick.c
+++ Src/SysTick/SysTick.c
@@ -7,14 +7,31 @@
 
 #include "SysTick_Types.h"
 #include "NVIC.h"
+#include "config.h"
 
 static dtSysTick *const SysTick = (dtSysTick*)0xE000E010;
 static uint32 Tick;
 
 void    SysTick_Init(uint32 ReloadValue);
+#if (OS_IS_USED == STD_OFF)
 uint32  SysTick_GetTicks(void);
+#endif
+#if (OS_IS_USED == STD_OFF)
 void    SysTick_Handler(void);
+#endif
 
+/** @brief initializes the SysTick module
+ *
+ * This function initializes and enables the SysTick.
+ * The SysTick is a 24 bit count down timer built into
+ * the ARM core. When the timer overflows a SysTick
+ * exception will be generated. The reload value is
+ * is loaded into the counter register at 0 at the
+ * counting starts from there down
+ *
+ * @param ReloadValue the reload value from which the
+ * counting starts
+ */
 void SysTick_Init(uint32 ReloadValue)
 {
     SysTick->CSR.Word = 0;
@@ -25,12 +42,29 @@ void SysTick_Init(uint32 ReloadValue)
     SysTick->CSR.Fields.Enable = 1;
 }
 
+#if (OS_IS_USED == STD_OFF)
+/** @brief return the value of 'Tick' variable
+ *
+ * Tick variable is increased by the exception handler
+ * of SysTick. This function can be used to access for
+ * read that variable.
+ *
+ * @return value of 'Tick'
+ */
 uint32 SysTick_GetTicks(void)
 {
     return Tick;
 }
+#endif
 
+#if (OS_IS_USED == STD_OFF)
+/** @brief Exception handler of SysTick
+ *
+ * Increases the 'Tick' variable so that that can be used
+ * to measure time
+ */
 void SysTick_Handler(void)
 {
     Tick++;
 }
+#endif
